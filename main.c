6e209076c90076521879da256de305adf8317ffa
mizunoyuuki <mizuno@careerindex.co.jp>
2026-07-06 01:21:42 +0900

    getline, free, 周りの理解を進める。db_tutorialの進捗

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+# include<stdlib.h>
+# include<stdio.h>
+# include<stdbool.h>
+# include<string.h>
+
 typedef struct InputBuffer InputBuffer;
 struct InputBuffer {
 	char* buffer;
@@ -16,13 +21,33 @@ InputBuffer* new_input_buffer() {
 
 void print_prompt() { printf("db > "); }
 
+void read_input(InputBuffer* input_buffer){
+	ssize_t bytes_read = 
+		getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+
+	if (bytes_read <= 1){
+		printf("Error reading input\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Ignore trailing newline
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer){
+	free(input_buffer->buffer);
+	free(input_buffer);
+}
+
 int main(int argc, char *argv[]){
 	InputBuffer* input_buffer = new_input_buffer();
 	while(true){
 		print_prompt();
-		read_input();
+		read_input(input_buffer);
 		
-		if(strcmp(input_buffer->bufer, ".exit") == 0){
+		if(strcmp(input_buffer->buffer, ".exit") == 0){
 			close_input_buffer(input_buffer);
 			exit(EXIT_SUCCESS);
 		} else {
